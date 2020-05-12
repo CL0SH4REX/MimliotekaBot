14f1a6150709323f0aba7c7ee771cd2f2c97d804
CL0SH4REX <62427956+CL0SH4REX@users.noreply.github.com>
2020-05-12 08:51:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+from discord.role import Role
+from discord.utils import get
+from discord.ext import commands
+import asyncio
+
+client = commands.Bot(command_prefix='!')
+role = Role
+Client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Ушао у ', client.user, 'ко циган у сестру')
+    activity = discord.Game(name="Молим се аллаху")
+    await client.change_presence(status=discord.Status.online, activity=activity)
+
+@client.command()
+async def on_message(ctx, message):
+    if message.author == client.user:
+        return
+
+@client.command()
+async def test(message):
+    test = '{0.author.mention} не '.format(message)
+    await message.channel.send(test)
+    print("""|      test	    |
+|---------------|""")
+
+@client.command()
+async def status(ctx):
+    id = client.get_guild(689559413555593235)
+    embed = discord.Embed(
+        title="Статус",
+        description= f"""Број муслимана у серверу је: {id.member_count}""",
+        color=discord.Color.green(),
+    )
+    await ctx.send(embed=embed)
+    print("""|     status	|
+|---------------|""")
+
+@client.command()
+@discord.ext.commands.has_role('Администратор')
+async def spuf(ctx, arg):
+    await ctx.send(arg)
+    
+@client.event
+async def on_member_join(member):
+    ment = member.mention
+    await client.get_channel(689573767969767533).send(f"{ment} је ушао у сервер!")
+    role = get(member.guild.roles, name="Обичан геј")
+    await member.add_roles(role)
+    print(f"""{member} је ушао у сервер и добио је "Чланови" рол!""")
+
+client.run(token)
